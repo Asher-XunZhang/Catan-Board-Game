49b1acbba86065dbe8089be6e8cbcaeb5f49e417
twiemold <twiemold@uvm.edu>
2021-10-14 13:12:07 -0400

    Added cities, settlements, roads

--- Player.py
+++ Player.py
@@ -1,9 +1,12 @@
 # Player class
 
 class Player:
-    def __init__(self, resource_cards, development_cards):
+    def __init__(self, resource_cards, development_cards, settlements, cities, roads):
         self.resource_cards = resource_cards
         self.development_cards = development_cards
+        self.settlements = settlements
+        self.cities = cities
+        self.roads = roads
 
     def get_resource_cards(self):
         return self.resource_cards
