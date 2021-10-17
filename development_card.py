99959cee7e8c7b9024bdcda3dacff880bee30be1
twiemold <twiemold@uvm.edu>
2021-10-17 11:12:28 -0400

    Created basic class definition

---
+++ development_card.py
@@ -0,0 +1,11 @@
+"""
+Development Card Class
+
+"""
+
+class DevelopmentCard:
+    def __init__(self, name, victory_points, description):
+        self.name = name
+        self.victory_points = victory_points
+        self.description = description
+
