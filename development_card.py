a6ae250f1db177ba0f692e47ab72093a9b349d5c
twiemold <twiemold@uvm.edu>
2021-10-17 11:16:59 -0400

    Added getter for victory points

--- development_card.py
+++ development_card.py
@@ -3,9 +3,12 @@ Development Card Class
 
 """
 
+
 class DevelopmentCard:
     def __init__(self, name, victory_points, description):
         self.name = name
         self.victory_points = victory_points
         self.description = description
 
+    def get_victory_points(self):
+        return self.victory_points
