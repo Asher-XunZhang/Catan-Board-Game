fa3c7828fd2a4cf0b3716ae3480459ec39119876
twiemold <twiemold@uvm.edu>
2021-10-26 13:31:18 -0400

    Created Player and laid out basic dice roll steps

--- components/window.py
+++ components/window.py
@@ -1,8 +1,18 @@
 import pygame
 from board import Board
 from random import *
+from player import Player
+# import Robber
 
-class window:
+
+def dice_roll():
+    seed(1)
+    value1 = randint(1, 6)
+    value2 = randint(1, 6)
+    return value1, value2
+
+
+class Window:
     def __init__(self):
         pygame.init()
         screen = pygame.display.set_mode((1280, 800))
@@ -10,6 +20,9 @@ class window:
         pygame.display.set_caption("Catan")
         board = Board(screen)
         hexes = board.hexes_infos()
+        # User input for player color, settlement locations
+        my_player = Player(0, 0, 0, 0, 0, [], [], [], [], 0, "blue")
+        # Create Robber
         pygame.display.flip()
         while True:
             for event in pygame.event.get():
@@ -24,16 +37,21 @@ class window:
                     xMouse = event.pos[0]
                     yMouse = event.pos[1]
                     print(xMouse, yMouse)
+                    num1, num2 = dice_roll()
+                    total = num1 + num2
+                    if total == 7:
+                        pass
+                        # Call Robber move function
+                    else:
+                        for tile in hexes:
+                            if tile.num == total:
+                                for settlement in tile.settlements:
+                                    pass
+                                    # settlement.owner.add_single_resources(tile.element)
+                                # Iterate through cities?
 
             pygame.display.update()
 
-    def dice_roll(self):
-        seed(1)
-        value1 = randint(1, 6)
-        value2 = randint(1, 6)
-        # if value == 7, call robber
-        return (value1, value2)
-
 
 if __name__ == '__main__':
-    game = window()
+    game = Window()
