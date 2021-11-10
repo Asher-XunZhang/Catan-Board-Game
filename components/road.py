ac061492f8e9297f2e26a99ad090795cd8bf8d33
DylanShelby <dshelby@uvm.edu>
2021-11-09 19:05:56 -0500

    Added check_click

--- components/road.py
+++ components/road.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Road:
 
     def __init__(self, owner, surface, start, end):
@@ -7,7 +8,16 @@ class Road:
         self.surface = surface
         self.start = start
         self.end = end
+        self.width = 4
 
     def draw_road(self):
-        points = {self.start, self.end} #The two points for the line
-        pygame.draw.polygon(self.surface, self.owner.color, points, 4)
+        pygame.draw.line(self.surface, self.owner.color, self.start, self.end, 5)
+
+    def check_click(self, x, y):
+        x_match = self.start < x < self.end + self.width
+        y_match = self.start < y < self.end + self.HEIGHT
+        if x_match and y_match:
+            return True
+        else:
+            return False
+
