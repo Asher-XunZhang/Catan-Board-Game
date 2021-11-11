d24e75826486c0ea85e1f816b13222369d3644ac
DylanShelby <dshelby@uvm.edu>
2021-11-11 15:07:12 -0500

    Commented out check_click for now

--- components/road.py
+++ components/road.py
@@ -11,16 +11,16 @@ class Road:
         self.width = 4
 
     def draw_road(self):
-        pygame.draw.line(self.surface, self.owner.color, self.start, self.end, 5)
+        pygame.draw.line(self.surface, self.owner.color, self.start, self.end, self.width)
 
     def assign_player(self, player):
         self.owner = player
 
-    def check_click(self, x, y):
-        x_match = self.start < x < self.end + self.width
-        y_match = self.start < y < self.end + self.HEIGHT
-        if x_match and y_match:
-            return True
-        else:
-            return False
+    # def check_click(self, x, y):
+    #     x_match = self.start < x < self.end + self.width
+    #     y_match = self.start < y < self.end + self.HEIGHT
+    #     if x_match and y_match:
+    #         return True
+    #     else:
+    #         return False
 
